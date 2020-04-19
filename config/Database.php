51db986f61429579910b6f58c8b2bec63c56f495
KalsonSaint <saintkalson@gmail.com>
2020-04-19 04:18:58 +0100

    API Updated

--- config/Database.php
+++ config/Database.php
@@ -22,7 +22,7 @@
             }
         }
 
-        // Method fetches everything included in the query parameter
+        // Method fetches everything included in the query parameter                                                          
         public function fetchAll($query){
             $stmt = $this->pdo->prepare($query);
             $stmt->execute();
@@ -48,20 +48,24 @@
 
         // Method keeps track of  Calls made to the API
         public function executeCall($username, $calls_allowed, $timeoutSecs) {
-            $query ="SELECT plan, calls_made, time_start, time_end FROM users WHERE username = '$username'";
+            $query ="SELECT plan, calls_made, time_start, time_end 
+                    FROM users 
+                    WHERE username = '$username'
+            ";
             $stmt=$this->pdo->prepare($query);
             $stmt->execute([$username]);
             $result = $stmt->fetch();
 
-            $timeOut = date(time()) - $result['time_start'] >= $timeOutSeconds || $result['time_start'] === 0;
+            //Set timeout to true if equal to Zero or isTimeOut
+            $timeOut = date(time()) - $result['time_start'] >= $timeOutSecs || $result['time_start'] === 0;
 
             // update calls made in respect to timeout
             $query = "UPDATE users SET calls_made";
-            $query .= $timeOut ? " 1, time_start = ".date(time()). " , time_end = ".strtotime("+ $timeOutSeconds seconds"): " calls_made + 1";
+            $query .= $timeOut ? " 1, time_start = ".date(time()). " , time_end = ".strtotime("+ $timeOutSecs seconds"): " calls_made + 1";
             $query .= "WHERE username = ?";
 
             $result['call_made'] = $timeOut ? 1 : $calls_made + 1;
-            $result['time_end'] = $timeOut ? strtotime(" + $timeOutSeconds seconds") : $result['time_end'];
+            $result['time_end'] = $timeOut ? strtotime(" + $timeOutSecs seconds") : $result['time_end'];
 
             // validate users subscription(plans)
             if($result['plan'] === 'unlimited'){
--- models/User.php
+++ models/User.php
@@ -1,1 +1,29 @@
 <!-- Performs CRUD Operation on Users table -->
+<?php
+
+class User{
+    private $db;
+
+    public function __construct(Database $db){
+        $this->db = $db;
+    }
+
+    public function insertUser($query, $parameters, $id){
+        if(isset($parameters->firstName) && isset($parameters->lastName) && isset($parameters->password)){
+            $firstName = $parameters->firstName;
+            $lastName = $parameters->lastName;
+            $password = $parameters->password;
+            $username = strtolower($firstName).strtolower($lastName).$id;
+            $this->db->insertUser($query,$firstName,$lastName,$password,$username);
+
+            return [
+                "firstName" => $firstName,
+                "lastNAme" => $lastName,
+                "username" => $username,
+                "api_key" => base64_encode("$username:$password"),
+            ];
+        } else{
+            return -1;
+        }
+    }
+}
