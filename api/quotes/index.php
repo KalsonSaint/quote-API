43f026704fec70823c962685bd67419bdd36ca5b
KalsonSaint <saintkalson@gmail.com>
2020-04-19 04:28:03 +0100

    API Updated

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,5 +1,12 @@
 <?php
-    require_once("../../config/Database.php");
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: GET,POST,PUT, DELETE");
+    header("Access-Control-Headers: Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
+
+    require_once "../../config/Database.php";
+    require_once "../../models/Quote.php";
+    require_once "../../models/HttpResponse.php";
 
     $db = new Database();
-?>
+    $quote = new Quote($db);
